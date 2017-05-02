00148c8115a2ebadac429ae7d1a7cdc9ddf76ab2
Carl Hain <crhain@gmail.com>
2017-05-02 13:37:47 -0500

    Add base tests and components for app and rename MainMenu to NavMenu

---
+++ src/interface/start-menu/StartMenu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './StartMenu.css';
+
+function StartMenu(props){    
+    return (        
+        <div id="start-menu">                            
+        </div>    
+    );
+}
+
+export default StartMenu;
